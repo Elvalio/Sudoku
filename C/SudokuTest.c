c5da0c9d4ec1b0c7bcf8ccc99b59bc574ebf4597
Elvalio <harry22510@gmail.com>
2023-11-26 14:04:30 +0100

    Code fini, erreurs corrigés, vérfication nécessaires, commentaires manquant

--- C/SudokuTest.c
+++ C/SudokuTest.c
@@ -6,15 +6,14 @@
 #define TAILLE N * N
 
 // Constantes
-const int CHIFFRE_MAX = 9 ;
-const int CHIFFRE_MIN = 1 ;
-const int CHIFFRE_VIDE = 0 ;
+const int CHIFFRE_MAX = 9;
+const int CHIFFRE_MIN = 1;
+const int CHIFFRE_VIDE = 0;
 
 // Typedef
-typedef int tGrille[TAILLE][TAILLE]; 
+typedef int tGrille[TAILLE][TAILLE];
 
 // Prototype / Entête
-
 void chargerGrille(tGrille g);
 void afficherGrille(tGrille g);
 int verifierGrillePleine(tGrille g);
@@ -22,65 +21,58 @@ void saisir(int *valeur);
 int possible(tGrille g, int colonne, int ligne, int valeur);
 
 // Main
-
-int main (){
-    tGrille grille1 ;
-    int numLigne, numColonne, valeur, grillePleine ;
+int main() {
+    tGrille grille1;
+    int numLigne, numColonne, valeur, grillePleine;
 
     chargerGrille(grille1);
-    while (grillePleine == 0)
-    {
+    while (grillePleine == 0) {
         afficherGrille(grille1);
         printf("Indices de la case ? \n");
         printf("Ligne : \n");
         saisir(&numLigne);
         printf("Colonne : \n");
         saisir(&numColonne);
-        if (grille1[numLigne - 1][numColonne - 1] != 0)
-        {
+        if (grille1[numLigne - 1][numColonne - 1] != 0) {
             printf("IMPOSSIBLE, la case n'est pas libre. \n");
-        }
-        else
-        {
+        } else {
             printf("Valeur à insérer ? \n");
+            printf(" %d %d ", numLigne - 1, numColonne - 1);
             saisir(&valeur);
-            if (possible(grille1, numColonne-1, numLigne-1, valeur))
-            {
-                grille1[numLigne-1][numColonne-1] = valeur ;
+            printf(" %d %d ", numLigne - 1, numColonne - 1);
+            if (possible(grille1, numColonne - 1, numLigne - 1, valeur)) {
+                grille1[numLigne - 1][numColonne - 1] = valeur;
+            } else {
+                printf("Impossible d'insérer la valeur à cet emplacement.\n");
             }
+            afficherGrille(grille1);
             grillePleine = verifierGrillePleine(grille1);
         }
-        
     }
-    afficherGrille(grille1);
     printf("Grille pleine, fin de partie \n");
 
     return EXIT_SUCCESS;
 }
 
-
-
 // Procédures / Fonctions
 
-void chargerGrille(tGrille g){
+void chargerGrille(tGrille g) {
     char nomFichier[30];
-    FILE * f;
+    FILE *f;
     printf("Nom du fichier ? \n");
     scanf("%s", nomFichier);
     f = fopen(nomFichier, "rb");
-    if (f==NULL){
+    if (f == NULL) {
         printf("\n ERREUR sur le fichier %s\n", nomFichier);
     } else {
-        fread(g, sizeof(int), TAILLE*TAILLE, f);
+        fread(g, sizeof(int), TAILLE * TAILLE, f);
         fclose(f);
     }
-    
 }
-
 
 void afficherGrille(tGrille g) {
     int rangeeAffichage;
-    
+
     printf("\n");
     printf("     1 2 3   4 5 6   7 8 9 \n");
     for (int rangee = 0; rangee < TAILLE; rangee++) {
@@ -99,7 +91,7 @@ void afficherGrille(tGrille g) {
             } else {
                 printf(" .");
             }
-        } 
+        }
         printf(" |\n");
     }
     printf("   +-------+-------+-------+ \n");
@@ -118,52 +110,38 @@ int verifierGrillePleine(tGrille g) {
     return grillePleine;
 }
 
-void saisir(int *valeur)
-{
-    char saisieUtilisateur[10] ;
+void saisir(int *valeur) {
+    char saisieUtilisateur[10];
     int saisieUtilisateurEntiere;
-    int saisieCorrecte = 0 ;
-    
-    do
-    {
+    int saisieCorrecte = 0;
+
+    do {
         scanf("%s", saisieUtilisateur);
-        if (sscanf(saisieUtilisateur, "%d", &saisieUtilisateurEntiere) != 0)
-        {
-            if ((saisieUtilisateurEntiere >= CHIFFRE_MIN) && (saisieUtilisateurEntiere <= TAILLE))
-            {
+        if (sscanf(saisieUtilisateur, "%d", &saisieUtilisateurEntiere) == 1) {
+            if (saisieUtilisateurEntiere >= CHIFFRE_MIN && saisieUtilisateurEntiere <= TAILLE) {
                 saisieCorrecte = 1;
                 *valeur = saisieUtilisateurEntiere;
+            } else {
+                printf("Entrez une valeur entière entre %d et %d \n", CHIFFRE_MIN, TAILLE);
             }
-            else
-            {
-                printf("L'entrée attendue est un chiffre entier compris entre %d et %d \n", CHIFFRE_MIN , TAILLE);
-            }
-            
+        } else {
+            printf("Entrez une valeur entière entre %d et %d \n", CHIFFRE_MIN, TAILLE);
         }
-        else
-        {
-            printf("L'entrée attendue est un chiffre entier compris entre %d et %d \n", CHIFFRE_MIN , TAILLE);
-        }
-    
-    } while (saisieCorrecte == 0);
 
+    } while (saisieCorrecte == 0);
 }
 
 int possible(tGrille g, int colonne, int ligne, int valeur) {
     // Vérification dans la ligne
-    for (int indiceColonne = 0; indiceColonne < TAILLE; indiceColonne++) {
-        if (g[ligne][indiceColonne] == valeur) {
-            printf("Erreur : La valeur ne peut pas être inséré dans cet emplacement\n");
-            printf("-> Valeur déjà dans la ligne à l'emplacement %d %d \n",ligne + 1 , indiceColonne + 1 );
+    for (int i = 0; i < TAILLE; i++) {
+        if (g[ligne][i] == valeur) {
             return 0;
         }
     }
 
     // Vérification dans la colonne
-    for (int indiceLigne = 0; indiceLigne < TAILLE; indiceLigne++) {
-        if (g[indiceLigne][colonne] == valeur) {
-            printf("Erreur : La valeur ne peut pas être inséré dans cet emplacement\n");
-            printf("-> Valeur déjà dans la colonne à l'emplacement %d %d\n",indiceLigne + 1 , colonne + 1);
+    for (int i = 0; i < TAILLE; i++) {
+        if (g[i][colonne] == valeur) {
             return 0;
         }
     }
@@ -171,12 +149,9 @@ int possible(tGrille g, int colonne, int ligne, int valeur) {
     // Vérification dans le bloc
     int debutBlocLigne = N * (ligne / N);
     int debutBlocColonne = N * (colonne / N);
-    for (int indiceLigneBloc = 0; indiceLigneBloc < N; indiceLigneBloc++) {
-        for (int indiceColonneBloc = 0; indiceColonneBloc < N; indiceColonneBloc++) {
-            if (g[debutBlocLigne + indiceLigneBloc][debutBlocColonne + indiceColonneBloc] == valeur) {
-                printf("Erreur : La valeur ne peut pas être inséré dans cet emplacement\n");
-                printf("-> Valeur déjà dans le bloc");
-                printf("-> Valeur déjà dans le bloc à l'emplacement %d %d\n",(debutBlocLigne + indiceLigneBloc) + 1, (debutBlocColonne + indiceColonneBloc) + 1 );
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (g[debutBlocLigne + i][debutBlocColonne + j] == valeur) {
                 return 0;
             }
         }
@@ -184,4 +159,3 @@ int possible(tGrille g, int colonne, int ligne, int valeur) {
 
     return 1;
 }
-
